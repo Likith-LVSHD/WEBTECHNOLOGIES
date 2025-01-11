bcfacf777e53def30a34d97d19d00d1a91ee74fe
Likith-LVSHD <151652135+Likith-LVSHD@users.noreply.github.com>
2025-01-11 09:36:41 +0530

    Add files via upload

---
+++ HW DAY 3/api.js
@@ -0,0 +1,47 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        document.getElementById("location").innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    document.getElementById("location").innerHTML = `Latitude: ${lat} <br> Longitude: ${lon}`;
+    getWeather(lat, lon);
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            document.getElementById("location").innerHTML = "User denied the request for Geolocation.";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            document.getElementById("location").innerHTML = "Location information is unavailable.";
+            break;
+        case error.TIMEOUT:
+            document.getElementById("location").innerHTML = "The request to get user location timed out.";
+            break;
+        case error.UNKNOWN_ERROR:
+            document.getElementById("location").innerHTML = "An unknown error occurred.";
+            break;
+    }
+}
+
+function getWeather(lat, lon) {
+    const apiKey = 'd227363787390d3a126e518ddb14699c'; 
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const weather = data.weather[0].description;
+            const temp = data.main.temp;
+            document.getElementById("weather").innerHTML = `Weather: ${weather} <br> Temperature: ${temp}°C`;
+        })
+        .catch(error => {
+            document.getElementById("weather").innerHTML = "Unable to retrieve weather data.";
+        });
+}
